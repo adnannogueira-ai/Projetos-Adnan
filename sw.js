e29dc073f9e2214291ca4d7721e84dadf7ea6899
adnannogueira-ai <adnannogueira@gmail.com>
2026-07-06 13:47:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* nG Solutions Croqui - sw */
-const CACHE="ng-croqui-v5";const ASSETS=["./","./index.html","./manifest.json"];
+const CACHE="ng-croqui-v6";const ASSETS=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match("./index.html"))));});
